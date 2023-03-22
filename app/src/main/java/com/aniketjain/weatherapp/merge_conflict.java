432f4690664837292b7e81ddf70bc31a6f30730a
minhazabrar <minhazabrar05@gmail.com>
2023-03-23 05:01:51 +0600

    Conflicted file created

---
+++ app/src/main/java/com/aniketjain/weatherapp/merge_conflict.java
@@ -0,0 +1,27 @@
+package com.aniketjain.weatherapp;
+
+public class merge_conflict {
+    int roll;
+    String name;
+
+    public merge_conflict(int roll, String name) {
+        this.roll = roll;
+        this.name = name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
