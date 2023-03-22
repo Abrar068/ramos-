eb8608ee7477988025bf837a68a4e253906ec4ca
minhazabrar <minhazabrar05@gmail.com>
2023-03-23 04:53:07 +0600

    new java class created

---
+++ app/src/main/java/com/aniketjain/weatherapp/ramos68.java
@@ -0,0 +1,27 @@
+package com.aniketjain.weatherapp;
+
+public class ramos68 {
+    Integer roll;
+    String name;
+
+    public ramos68(Integer roll, String name) {
+        this.roll = roll;
+        this.name = name;
+    }
+
+    public Integer getRoll() {
+        return roll;
+    }
+
+    public void setRoll(Integer roll) {
+        this.roll = roll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
